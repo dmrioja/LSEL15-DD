f5982b08533b0dcaec7dd623e163c1ade3e716c8
Samuel Lopez <user@lsel-2014.(none)>
2015-04-09 14:13:52 +0200

    Funcionando sin tiempos

--- maqcafe/fsm.c
+++ maqcafe/fsm.c
@@ -16,35 +16,33 @@
 #include <stdio.h>
 #include "fsm.h"
 
+
 /* Constructor function */
-fsm_t*
-fsm_new (fsm_trans_t *tt) {
+fsm_t* fsm_new (fsm_trans_t *tt) {
 	fsm_t *this = (fsm_t*) malloc (sizeof (fsm_t));
 	fsm_init (this, tt);
 	return this;
 }
 
 /* Initialization function */
-void
-fsm_init (fsm_t *this, fsm_trans_t *tt) {
+void fsm_init (fsm_t *this, fsm_trans_t *tt) {
 	this->tt = tt;
 }
 
 /* FSM execution function */
-void
-fsm_run (fsm_t *this) {
-	fsm_trans_t *t;
-printf("%d", this->tt->last_state);
-printf("%d", sizeof(this->tt));
-	for (t = this->tt; t < this->tt + sizeof(this->tt); t++) {
-/*		if ((this->current_state == t->last_state) && t->input(this)) {
+void fsm_run (fsm_t *this) {
+  fsm_trans_t *t;
+	t = this->tt;
+	int estado = this->current_state;
+	for (t = this->tt; t->last_state >= 0; ++t) {
+		if ((this->current_state == t->last_state) && t->input(this)) {
 			this->current_state = t->next_state;
+			if (t->last_state != t->next_state) {
+				printf(" ** Cambio de estado **");
+			}
 			if (t->output)
 				t->output(this);
 			break;
-		}*/
-	printf("\ntaka");
+		}
 	}
 }
-
-
--- maqcafe/maqcafe.c
+++ maqcafe/maqcafe.c
@@ -14,6 +14,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "fsm.h"
 
 /* Definir aquí flags para el modo DEBUG */ 
@@ -32,116 +33,157 @@
 #define PITIDO 6
 
 /* MAQCAFE TIMERS */
-#define VASO_TIMER 10
-#define CUCHARA_TIMER 10
-#define AZUCAR_TIMER 10
-#define CAFE_TIMER 10
-#define LECHE_TIMER 10
-#define PITIDO_TIMER 10
+#define VASO_TIMER 3
+#define CUCHARA_TIMER 4
+#define AZUCAR_TIMER 5
+#define CAFE_TIMER 6
+#define LECHE_TIMER 7
+#define PITIDO_TIMER 8
 
 int timers[6] = {VASO_TIMER, CUCHARA_TIMER, AZUCAR_TIMER,
 		 CAFE_TIMER, LECHE_TIMER, PITIDO_TIMER};
 
 /* GLOBAL VARIABLES */
-/* */
 int moneda = 0;
 int botonDevolver = 0;
 int botonCafe = 0;
 int fin = 0;
 int cuenta = 0;
 int timer = 0;
+struct timespec tim;
 
 
 /* MONEDERO INPUT FUNCTIONS */
-int checkMoneda(fsm_t *this) {
+static int checkCuenta(fsm_t *this) {
 	if (moneda != 0) {
 		cuenta += moneda;
 		moneda = 0;
 		return 1;
+	} else {
+		return 0;
 	}
-	return 0;
 }
-
-int checkBotonDevolver(fsm_t *this) {
+static int checkBotonDevolver(fsm_t *this) {
 	if (botonDevolver != 0) {
 		return 1;
+	} else {
+		return 0;
 	}
-	return 0;
 }
 
-int checkBotonCafe(fsm_t *this) {
+static int checkBotonCafe(fsm_t *this) {
 	if((botonCafe != 0) && (cuenta >= 50)) {
 		cuenta -= 50;
 		return 1;
+	} 
+	return 0;
+}
+
+static int checkFin(fsm_t *this) {
+	if (fin == 1) {
+		return 1;
 	}
 	return 0;
 }
 
-int checkFin(fsm_t *this) {
-	return fin;
-}
-
-int checkNoFin(fsm_t *this){
-	return !fin;
+static int checkNoFin(fsm_t *this){
+	if (fin == 0) {
+		return 1;
+	}	
+	return 0;
 }
 
 /* MAQCAFE INPUT FUNCTIONS */
 
-
-int checkTimer(fsm_t *this) {
-	if(timer == 0) {
+static int checkTimer(fsm_t *this) {
+	if((timer-1) <= 0) {
 		return 1;
 	}
 	else {
+		/* nanosleep(&tim, NULL);*/
 		timer--;
 		return 0;
 	}
 }
 
-/* INPUT FUNCTIONS COLECTION */
-int (*inputsMonedero[4])() = {checkMoneda, checkBotonDevolver, checkBotonCafe, checkFin};
-int (*inputsMaqCafe[3])() = {checkFin, checkNoFin, checkTimer};
-
-
-
 /* OUTPUT SIGNAL GENERATIONS */
 
-void doDevolver(fsm_t *this) {
-	printf("\nDevolución: %d céntimos", cuenta);
+static void doDevolver(fsm_t *this) {
+	printf(" ++ Cambio %d cént ++", cuenta);
 	cuenta = 0;
 }
 
-void finOn(fsm_t *this) {
+static void finOn(fsm_t *this) {
 	fin = 1;
 }
 
-void finOff(fsm_t *this) {
+static void finOff(fsm_t *this) {
 	fin = 0;
 }
 
-void restartTimer(fsm_t *this){
-
-/* En la funcion run primero se cambia de estado y luego se llama a las funciones output
- asi que hay que tener cuidado al definir esto
- por eso se pone estate -1*/
-
-timer = timers[this->current_state-1];
+static void restartTimer(fsm_t *this){
+/* El primer estado en el que se usa el timer es 1, 
+   el array comienza en 0 */
+	
+	timer = timers[this->current_state-1];
 }
 
-void doNothing(fsm_t *this) {
+static void doNothing(fsm_t *this) {
 	return;
 }
 
-/* OUTPUT FUNCTIONS COLECTION */
-void (*outputsMonedero[3])() = {doDevolver, finOff, doNothing};
-void (*outputsMaqCafe[3])() = {finOn, restartTimer, doNothing};
 
+
+
+/* TIME CALC FUNCTIONS */
+
+/* long diff(struct timespec *start, struct timespec *end) { */
+/*         struct timespec *temp; */
+/*         if ((end.tv_nsec - start.tv_nsec) < 0) { */
+/* 		temp.tv_nsec = 1000000000 + end.tv_nsec - start.tv_nsec; */
+/* 	}  */
+/* 	else { */
+/* 		temp.tv_nsec = end.tv_nsec - start.tv_nsec; */
+/*         } */
+/*         return temp.tv_nsec; */
+/* } */
 
 /* EXECUTION OF MAQUINA CAFE FSM */
 
-int
-main(void){
-  /* INIT VARIABLES */
+static fsm_trans_t tt_monedero[] = {
+	{REPOSO, checkCuenta, REPOSO , doNothing} , 
+	{REPOSO,  checkBotonDevolver, REPOSO , doDevolver} ,  
+	{REPOSO,  checkBotonCafe, SIRVIENDO , finOff} ,
+	{SIRVIENDO, checkFin, REPOSO , doDevolver} ,
+	{SIRVIENDO, checkCuenta, SIRVIENDO , doNothing} ,
+	{-1, NULL, -1, NULL} ,
+};
+
+static fsm_trans_t tt_maqcafe[] = {  
+	{IDLE, checkFin, IDLE , doNothing} ,  
+	{IDLE, checkNoFin, VASO , restartTimer} ,  
+	{VASO, checkTimer, CUCHARA , restartTimer} , 
+	{CUCHARA, checkTimer, AZUCAR , restartTimer} , 
+	{AZUCAR, checkTimer, CAFE , restartTimer} ,  
+	{CAFE, checkTimer, LECHE , restartTimer} ,  
+	{LECHE, checkTimer, PITIDO , restartTimer} , 
+	{PITIDO, checkTimer, IDLE , finOn} ,
+	{-1, NULL, -1, NULL} ,
+};
+
+int main(void){
+
+	/* TIME VARIABLES */
+
+	struct timespec *t1, *t2, *t3;
+	long task1, task2;
+	long maxTask1, maxTask2;
+	task1 = task2 = maxTask1 = maxTask2 = 0;
+	/* tim.tv_sec = 0; */
+	/* tim.tv_nsec = 10000000;  */
+	/* 10 ms */
+
+	/* INIT VARIABLES */
 	
 	moneda = 0;
 	cuenta = 0;
@@ -149,66 +191,45 @@ main(void){
 	botonCafe = 0;
 	fin = 1; /*Fin tiene que empezar en 1 para poner 0 cuando esté sirviendo*/
 	
-	fsm_trans_t tt_monedero[] = {
-		{REPOSO, inputsMonedero[0], REPOSO , outputsMonedero[2]} , 
-		{REPOSO,  inputsMonedero[1], REPOSO , outputsMonedero[0]} ,  
-		{REPOSO,  inputsMonedero[2], SIRVIENDO ,outputsMonedero[1] } ,/*hay que avisar de que hemos cambiado de estado a sirviendo+*/
-		{SIRVIENDO, inputsMonedero[3], REPOSO , outputsMonedero[0]} ,
-	};
-	
-	fsm_trans_t tt_maqcafe[] = {  
-		{IDLE, inputsMaqCafe[0], IDLE , outputsMaqCafe[2]} ,  
-		{IDLE, inputsMaqCafe[1], VASO , outputsMaqCafe[1]} ,  
-		{VASO, inputsMaqCafe[2], CUCHARA , outputsMaqCafe[1]} , 
-		{CUCHARA, inputsMaqCafe[2], AZUCAR , outputsMaqCafe[1]} , 
-		{AZUCAR, inputsMaqCafe[2], CAFE , outputsMaqCafe[1]} ,  
-		{CAFE, inputsMaqCafe[2], LECHE , outputsMaqCafe[1]} ,  
-		{LECHE, inputsMaqCafe[2], PITIDO , outputsMaqCafe[1]} , 
-		{PITIDO, inputsMaqCafe[2], IDLE , outputsMaqCafe[0]} ,
-	};	
-	
-	
 	fsm_t *monedero = fsm_new(tt_monedero);
 	fsm_t *maqcafe = fsm_new(tt_maqcafe);
+
+	monedero->current_state = 0;
 	
-/*
-	FILE *estimulos;
-	char textEstim[100];
-	 Lectura fichero de estímulos 
-	estimulos = fopen("test.txt","r");
-	if (estimulos == NULL) {
-		printf( "No se puede abrir el fichero.\n" );
-		exit( 1 );
-	}*/
-printf("1");
 	while (scanf("%d %d %d", &moneda, &botonCafe, &botonDevolver) == 3) {
-		printf("2");
-		fsm_run(maqcafe);
-printf("3");
+		/* INTERFAZ */
+		printf("\n>>> Mon: %d ",moneda);
+		printf("Caf: %d ", botonCafe);
+		printf("Dev: %d ", botonDevolver);
+		printf("Cue: %d ",cuenta);
+		printf("Fin: %d",fin);		
+		printf("Est1: %d ",monedero->current_state);
+		printf("Est2: %d ",maqcafe->current_state);
+		
+		/* EJECUCION DE LAS FSM */
+		/* clock_gettime(CLOCK_MONOTONIC, t1); */
 		fsm_run(monedero);
-printf("4");
-	}
-printf("5");
-	
-	/*printf( "\nContenido del fichero:\n" );
-	fgets(textEstim,100,estimulos);
-	while (feof(estimulos) == 0) {
-		printf( "%s",textEstim );
-		fgets(textEstim,100,estimulos);
+		/* clock_gettime(CLOCK_MONOTONIC, t2); */
+		fsm_run(maqcafe);
+		/* clock_gettime(CLOCK_MONOTONIC, t3); */
 
+		/* task1 = diff(t1,t2); */
+		/* task2 = diff(t2,t3); */
 
-		
-		fsm_run(p_monedero);
-		  fsm_run(p_maqcafe);
-		  delay until next action
+		if(task1 > maxTask1){
+			maxTask1 = task1;
+		}
+
+		if(task2 > maxTask2){
+			maxTask2 = task2;
+		}
 		
 	}
-	
-	if (fclose(estimulos) != 0) {
-		printf( "Problemas al cerrar el fichero\n" );
-	}*/
-	
 	printf("\n\nFin de ejecución");
+	printf("\nMaxTime Monedero: %ld", maxTask1);
+	printf("\nMaxTime MaqCafe: %ld", maxTask2);
+
 	return 0;
+
 }
 //EOF
